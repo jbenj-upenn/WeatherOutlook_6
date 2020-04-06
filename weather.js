424ab9c76544d2228914768f1ac211b1042a2fdc
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-04-05 23:01:24 -0400

    working on a new HTML utilizing Bootstrap; working on styling and content; preparing to implement jQuery, JSON, and AJAX

--- weather.js
+++ weather.js
@@ -6,8 +6,54 @@
 
 // openweathermap.org API w city, state, and country code: api.openweathermap.org/data/2.5/forecast?q={city name},{state},{country code}&appid=3a1badee57acecc35569f0a56b894c72
 
-var weatherSearch = $("weatherSearch");
+// //$("#enter").on("click", function () {
+//     event.preventDefault()
+//     console.log("weather in your city");
 
-function buildQueryURL() {
+// $("#enter");
+
+//     var searchWeather = $("searchWeather").val();
+
+//     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + queryURL + "&appid=3a1badee57acecc35569f0a56b894c72";
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     });
+
+
+
+
+// function buildQueryURL() {
     
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q= " + cityName + stateName + countryCode + " &appid=3a1badee57acecc35569f0a56b894c72";
+    
+// };
+
+//----------------------------------START OVER---------------------------------------//
+
+$(document).ready(function(){
+
+    $('#getWeather').click(function(){
+
+        var city = $("#cityWeather").val();
+
+        if(city != ''){
+
+            $.ajax({
+                url: "https://api.openweathermap.org/data/2.5/weather?q= " + city + "&units=imperial" + "&appid=3a1badee57acecc35569f0a56b894c72",
+                method: "GET",
+                dataType: "jsonp",
+                success: function(data){
+                    console.log(data);
+
+                }
+
+            });
+
+        }else{
+            $("#error").html("Fill in this Field, Fool!");
+    }
+
+});
+
+});
