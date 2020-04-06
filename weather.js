b270b1354918e17bbe021d0dfe3698b57efa9cc8
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-04-06 03:14:37 -0400

    lots of research and work on the basics; still working on figuring out how to make search results persist

--- weather.js
+++ weather.js
@@ -25,35 +25,51 @@
 
 
 // function buildQueryURL() {
-    
-    
+
+
 // };
 
-//----------------------------------START OVER---------------------------------------//
+//----------------------------------BEGIN---------------------------------------//
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('#getWeather').click(function(){
+    $('#getWeather').click(function () {
 
         var city = $("#cityWeather").val();
 
-        if(city != ''){
+        if (city != '') {
 
             $.ajax({
                 url: "https://api.openweathermap.org/data/2.5/weather?q= " + city + "&units=imperial" + "&appid=3a1badee57acecc35569f0a56b894c72",
                 method: "GET",
                 dataType: "jsonp",
-                success: function(data){
+                success: function (data) {
                     console.log(data);
+                    var widget = display(data);
 
+                    $("#showWeather").html(widget);
+
+                    //empty the input field after the user clicks search
+                    $("#cityWeather").val('');
                 }
 
             });
 
-        }else{
+        } else {
             $("#error").html("Fill in this Field, Fool!");
-    }
+        }
+
+    });
 
 });
 
-});
+function display(data) {
+    return "<h2 'font-size:45px; font-weight: bold;' class='text-center'>You're Locked in to the Weather in " + data.name + " in that " + data.sys.country + " (country code...figure it out) </h2>" +
+        "<h3 style='padding-left:35px;'><strong>What's Coming from the Sky?</strong>:" + data.weather[0].main + "</h3>" +
+        "<h3 style='padding-left:35px;'><strong>Further Info on That?</strong>: <img src='http://openweathermap.org/img/wn/"+ data.weather[0].icon + ".png'> " + data.weather[0].description + "</h3>" +
+        "<h3 style='padding-left:35px;'><strong>How's that Thermostat Looking?</strong>: " + data.main.temp + " &deg;F</h3>" +
+        "<h3 style='padding-left:35px;'><strong>Gimme a Range: What's tshe Max?</strong>: " + data.main.temp_max + " &deg;F</h3>" +
+        "<h3 style='padding-left:35px;'><strong>How about the Min?</strong>: " + data.main.temp_min + " &deg;F</h3>" +
+        "<h3 style='padding-left:35px;'><strong>Am I Gonna Be Sweating in that Humidity? </strong>: " + data.main.humidity + "% </h3>" +
+        "<h3 style='padding-left:35px;'><strong>Any mini-Tornados Happening? What's the Wind Speed?</strong>: " + data.wind.speed + " MpH</h3>";
+};
